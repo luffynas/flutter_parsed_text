7f427a930baf7365bdbe6e41664345e0d4ade462
Minh Đại <minhdai@Minhs-MacBook-Pro.local>
2021-08-05 18:23:53 +0700

    add read more, less

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -23,166 +23,65 @@ class MainApp extends StatefulWidget {
 }
 
 class _MainAppState extends State<MainApp> {
+  List<MatchText> pareText() {
+    return [
+      MatchText(
+        type: ParsedType.EMAIL,
+        style: TextStyle(
+          color: Colors.red,
+          fontSize: 15,
+        ),
+        onTap: (url) {
+          print('tap $url');
+        },
+      ),
+      MatchText(
+        pattern: r"\B@+([\w]+)\b",
+        style: TextStyle(
+          color: Colors.amber,
+          fontSize: 15,
+        ),
+        renderText: ({required String str, required String pattern}) {
+          Map<String, String> map = Map<String, String>();
+          RegExp customRegExp = RegExp(pattern);
+          Match match = customRegExp.firstMatch(str) as Match;
+          map['display'] = match.group(1)!;
+          map['value'] = match.group(2)!;
+          return map;
+        },
+        onTap: (url) {
+          print('tap $url');
+        },
+      ),
+      MatchText(
+        pattern: r"\B#+([\w]+)\b", // a custom pattern to match
+        style: TextStyle(
+          color: Colors.green,
+          fontSize: 15,
+        ), // custom style to be applied to this matched text
+        onTap: (url) {
+          print('tap $url');
+        }, // callback funtion when the text is tapped on
+      ),
+    ];
+  }
+
+  String longText =
+      "Our fun and flirty embroidered dress with godet skirt detail is a great choice for a mother of the bride, guest of #wedding, or special occasion. As a midi length #dress with 3/4 illusion #sleeves and illusion neckline it provides great coverage while not being stifling.❤️❤❤";
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: <Widget>[
-          SizedBox(
-            height: 40.0,
-          ),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: ParsedText(
-              selectable: true,
-              alignment: TextAlign.start,
-              // regexOptions: RegexOptions(
-              //   caseSensitive: false,
-              //   multiLine: true,
-              //   dotAll: false,
-              //   unicode: false,
-              // ),
-              text:
-                  "[@michael:51515151] Hello  --- spoiler ---\n\n spoiler content \n--- spoiler ---\n  https://172.0.0.1 london this is https://apps.apple.com/id/app/facebook/id284882215  an example of the ParsedText, links like http://www.google.com or http://www.facebook.com are clickable and phone number 444-555-6666 can call too. But you can also do more with this package, for example Bob will change style and David too.\nAlso a US number example +1-(800)-831-1117. foo@gmail.com And the magic number is 42! #flutter #flutterdev",
-              parse: <MatchText>[
-                MatchText(
-                    type: ParsedType.EMAIL,
-                    style: TextStyle(
-                      color: Colors.red,
-                      fontSize: 24,
-                    ),
-                    onTap: (url) {
-                      launch("mailto:" + url);
-                    }),
-                MatchText(
-                    type: ParsedType.URL,
-                    style: TextStyle(
-                      color: Colors.blue,
-                      fontSize: 24,
-                    ),
-                    onTap: (url) async {
-                      var a = await canLaunch(url);
-
-                      if (a) {
-                        launch(url);
-                      }
-                    }),
-                MatchText(
-                    type: ParsedType.PHONE,
-                    style: TextStyle(
-                      color: Colors.red,
-                      fontSize: 24,
-                    ),
-                    onTap: (url) {
-                      launch("tel:" + url);
-                    }),
-                MatchText(
-                  type: ParsedType.CUSTOM,
-                  pattern:
-                      r"^(?:http|https):\/\/[\w\-_]+(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)",
-                  style: TextStyle(color: Colors.lime),
-                  onTap: (url) => print(url),
-                ),
-                MatchText(
-                    type: ParsedType.CUSTOM,
-                    pattern:
-                        "(---( )?(`)?spoiler(`)?( )?---)\n\n(.*?)\n( )?(---( )?(`)?spoiler(`)?( )?---)",
-                    style: TextStyle(
-                      color: Colors.purple,
-                      fontSize: 50,
-                    ),
-                    onTap: (url) {
-                      launch("tel:" + url);
-                    }),
-                MatchText(
-                  pattern: r"\[(@[^:]+):([^\]]+)\]",
-                  style: TextStyle(
-                    color: Colors.green,
-                    fontSize: 24,
-                  ),
-                  renderText: ({required pattern, required str}) {
-                    RegExp customRegExp = RegExp(r"\[(@[^:]+):([^\]]+)\]");
-                    Match match = customRegExp.firstMatch(str)!;
-
-                    print('test test: ${match[1]}');
-                    // return Container(
-                    //   padding: EdgeInsets.all(5.0),
-                    //   color: Colors.amber,
-                    //   child: Text(match[1]!),
-                    // );
-                    //
-                    return {'display': match[1]!};
-                  },
-                  onTap: (url) {
-                    showDialog(
-                      context: context,
-                      builder: (BuildContext context) {
-                        Map<String, String> map = Map<String, String>();
-                        RegExp customRegExp = RegExp(r"\[(@[^:]+):([^\]]+)\]");
-                        Match match = customRegExp.firstMatch(url)!;
-                        // return object of type Dialog
-                        return AlertDialog(
-                          title: new Text("Mentions clicked"),
-                          content: new Text("${match.group(1)!} clicked."),
-                          actions: <Widget>[
-                            // usually buttons at the bottom of the dialog
-                            new FlatButton(
-                              child: new Text("Close"),
-                              onPressed: () {},
-                            ),
-                          ],
-                        );
-                      },
-                    );
-                  },
-                  // onLongTap: (url) {
-                  //   print('long press');
-                  // },
-                ),
-                MatchText(
-                  pattern: r"\B#+([\w]+)\b",
-                  style: TextStyle(
-                    color: Colors.pink,
-                    fontSize: 24,
-                  ),
-                  onTap: (url) async {
-                    showDialog(
-                      context: context,
-                      builder: (BuildContext context) {
-                        // return object of type Dialog
-                        return AlertDialog(
-                          title: new Text("Hashtag clicked"),
-                          content: new Text("$url clicked."),
-                          actions: <Widget>[
-                            // usually buttons at the bottom of the dialog
-                            new FlatButton(
-                              child: new Text("Close"),
-                              onPressed: () {},
-                            ),
-                          ],
-                        );
-                      },
-                    );
-                  },
-                  // onLongTap: (url) {
-                  //   print('long press');
-                  // },
-                ),
-                MatchText(
-                    pattern: r"lon",
-                    style: TextStyle(
-                      color: Colors.pink,
-                      fontSize: 24,
-                    ),
-                    onTap: (url) async {})
-              ],
-              style: TextStyle(
-                fontSize: 24,
-                color: Colors.black,
-              ),
-            ),
-          )
-        ],
+      appBar: AppBar(
+        title: Text("AppBar Title"),
+      ),
+      body: ParsedText(
+        trimLines: 3,
+        parse: pareText(),
+        text: longText,
+        alignment: TextAlign.start,
+        overflow: TextOverflow.clip,
+        style: TextStyle(color: Colors.black),
       ),
     );
   }
--- lib/src/parsed_text.dart
+++ lib/src/parsed_text.dart
@@ -1,7 +1,11 @@
 part of flutter_parsed_text;
 
-/// Parse text and make them into multiple Flutter Text widgets
-class ParsedText extends StatelessWidget {
+enum TrimMode {
+  Length,
+  Line,
+}
+
+class ParsedText extends StatefulWidget {
   /// If non-null, the style to use for the global text.
   ///
   /// It takes a [TextStyle] object as it's property to style all the non links text objects.
@@ -74,149 +78,361 @@ class ParsedText extends StatelessWidget {
   /// [text] paramtere should not be null and is always required.
   /// If the [style] argument is null, the text will use the style from the
   /// closest enclosing [DefaultTextStyle].
-  ParsedText({
+  ///
+  final int trimLength;
+  final int trimLines;
+  final TrimMode trimMode;
+  final TextStyle moreStyle;
+  final TextStyle lessStyle;
+  final Function(bool val)? callback;
+  final String delimiter;
+  final String trimExpandedText;
+  final String trimCollapsedText;
+  final Color? colorClickableText;
+  final TextAlign? textAlign;
+  final Locale? locale;
+  final String? semanticsLabel;
+  final TextStyle? delimiterStyle;
+  final bool isLoadMore;
+
+  const ParsedText({
     Key? key,
-    required this.text,
-    this.parse = const <MatchText>[],
+    this.trimExpandedText = 'Show less',
+    this.trimCollapsedText = 'Show more',
+    this.colorClickableText,
+    this.trimLength = 240,
+    this.trimLines = 2,
+    this.trimMode = TrimMode.Line,
     this.style,
-    this.alignment = TextAlign.start,
+    this.textAlign,
     this.textDirection,
+    this.locale,
+    this.textScaleFactor = 1.0,
+    this.semanticsLabel,
+    this.moreStyle = const TextStyle(color: Colors.blue),
+    this.lessStyle = const TextStyle(color: Colors.red),
+    this.delimiter = _kEllipsis + ' ',
+    this.delimiterStyle,
+    this.callback,
+    this.regexOptions = const RegexOptions(),
+    this.parse = const <MatchText>[],
+    this.alignment = TextAlign.start,
     this.softWrap = true,
     this.overflow = TextOverflow.clip,
-    this.textScaleFactor = 1.0,
     this.strutStyle,
     this.textWidthBasis = TextWidthBasis.parent,
     this.maxLines,
     this.onTap,
     this.selectable = false,
-    this.regexOptions = const RegexOptions(),
+    this.isLoadMore = true,
+    required this.text,
   }) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
-    // Seperate each word and create a new Array
-    String newString = text;
+  ParsedTextState createState() => ParsedTextState();
+}
 
-    Map<String, MatchText> _mapping = Map<String, MatchText>();
+const String _kEllipsis = '\u2026';
 
-    parse.forEach((e) {
-      if (e.type == ParsedType.EMAIL) {
-        _mapping[emailPattern] = e;
-      } else if (e.type == ParsedType.PHONE) {
-        _mapping[phonePattern] = e;
-      } else if (e.type == ParsedType.URL) {
-        _mapping[urlPattern] = e;
-      } else {
-        _mapping[e.pattern!] = e;
-      }
+const String _kLineSeparator = '\u2028';
+
+/// Parse text and make them into multiple Flutter Text widgets
+class ParsedTextState extends State<ParsedText> {
+  bool _readMore = true;
+
+  void _onTapLink() {
+    setState(() {
+      _readMore = !_readMore;
     });
+  }
 
-    final pattern = '(${_mapping.keys.toList().join('|')})';
+  @override
+  Widget build(BuildContext context) {
+    String newString = widget.text;
 
-    List<InlineSpan> widgets = [];
+    final DefaultTextStyle defaultTextStyle = DefaultTextStyle.of(context);
+    TextStyle? effectiveTextStyle = widget.style;
+    if (widget.style?.inherit ?? false) {
+      effectiveTextStyle = defaultTextStyle.style.merge(widget.style);
+    }
 
-    newString.splitMapJoin(
-      RegExp(
-        pattern,
-        multiLine: regexOptions.multiLine,
-        caseSensitive: regexOptions.caseSensitive,
-        dotAll: regexOptions.dotAll,
-        unicode: regexOptions.unicode,
-      ),
-      onMatch: (Match match) {
-        final matchText = match[0];
+    final textAlign =
+        widget.textAlign ?? defaultTextStyle.textAlign ?? TextAlign.start;
+    final textDirection = widget.textDirection ?? Directionality.of(context);
+    final textScaleFactor = widget.textScaleFactor;
+    final overflow = defaultTextStyle.overflow;
+    final locale = widget.locale ?? Localizations.maybeLocaleOf(context);
+    final _defaultDelimiterStyle = widget.delimiterStyle ?? effectiveTextStyle;
 
-        final mapping = _mapping[matchText!] ??
-            _mapping[_mapping.keys.firstWhere((element) {
-              final reg = RegExp(
-                element,
-                multiLine: regexOptions.multiLine,
-                caseSensitive: regexOptions.caseSensitive,
-                dotAll: regexOptions.dotAll,
-                unicode: regexOptions.unicode,
+    TextSpan _delimiter = TextSpan(
+      text: _readMore
+          ? widget.trimCollapsedText.isNotEmpty
+              ? widget.delimiter
+              : ''
+          : '',
+      style: _defaultDelimiterStyle,
+      recognizer: TapGestureRecognizer()..onTap = _onTapLink,
+    );
+
+    Widget result = LayoutBuilder(
+        builder: (BuildContext context, BoxConstraints constraints) {
+      assert(constraints.hasBoundedWidth);
+      bool isShowReadMore = true;
+
+      final double maxWidth = constraints.maxWidth;
+
+      // Create a TextSpan with data
+      final text = TextSpan(
+        style: effectiveTextStyle,
+        text: widget.text,
+      );
+
+      // Layout and measure link
+      TextPainter textPainter = TextPainter(
+        text: _delimiter,
+        textAlign: textAlign,
+        textDirection: textDirection,
+        textScaleFactor: textScaleFactor,
+        maxLines: widget.trimLines,
+        ellipsis: overflow == TextOverflow.ellipsis ? widget.delimiter : null,
+        locale: locale,
+      );
+      textPainter.layout(minWidth: 0, maxWidth: maxWidth);
+      final linkSize = textPainter.size;
+
+      textPainter.layout(minWidth: 0, maxWidth: maxWidth);
+      final delimiterSize = textPainter.size;
+
+      // Layout and measure text
+      textPainter.text = text;
+      textPainter.layout(minWidth: constraints.minWidth, maxWidth: maxWidth);
+      final textSize = textPainter.size;
+
+      // Get the endIndex of data
+      bool linkLongerThanLine = false;
+      int endIndex;
+      if (linkSize.width < maxWidth) {
+        final readMoreSize = linkSize.width + delimiterSize.width;
+        final pos = textPainter.getPositionForOffset(Offset(
+          textDirection == TextDirection.rtl
+              ? readMoreSize
+              : textSize.width - readMoreSize,
+          textSize.height,
+        ));
+        endIndex = textPainter.getOffsetBefore(pos.offset) ?? 0;
+      } else {
+        var pos = textPainter.getPositionForOffset(
+          textSize.bottomLeft(Offset.zero),
+        );
+        endIndex = pos.offset;
+        linkLongerThanLine = true;
+      }
+
+      switch (widget.trimMode) {
+        case TrimMode.Length:
+          if (widget.trimLength < widget.text.length) {
+            newString = _readMore
+                ? widget.text.substring(0, endIndex) +
+                    (linkLongerThanLine ? _kLineSeparator : '...')
+                : widget.text;
+          } else {
+            isShowReadMore = false;
+
+            newString = widget.text;
+          }
+          break;
+        case TrimMode.Line:
+          if (textPainter.didExceedMaxLines) {
+            newString = _readMore
+                ? widget.text.substring(0, endIndex) +
+                    (linkLongerThanLine ? _kLineSeparator : '...')
+                : widget.text;
+          } else {
+            isShowReadMore = false;
+
+            newString = widget.text;
+          }
+          break;
+        default:
+          newString = widget.text;
+      }
+
+      // Seperate each word and create a new Array
+
+      TextStyle? style = widget.style;
+
+      Map<String, MatchText> _mapping = Map<String, MatchText>();
+
+      widget.parse.forEach((e) {
+        if (e.type == ParsedType.EMAIL) {
+          _mapping[emailPattern] = e;
+        } else if (e.type == ParsedType.PHONE) {
+          _mapping[phonePattern] = e;
+        } else if (e.type == ParsedType.URL) {
+          _mapping[urlPattern] = e;
+        } else {
+          _mapping[e.pattern!] = e;
+        }
+      });
+
+      final pattern = '(${_mapping.keys.toList().join('|')})';
+
+      List<InlineSpan> widgets = [];
+      var isMultiLine = widget.regexOptions.multiLine;
+      var isCaseSensitive = widget.regexOptions.caseSensitive;
+      var isDotAll = widget.regexOptions.dotAll;
+      var isUnicode = widget.regexOptions.unicode;
+
+      newString.splitMapJoin(
+        RegExp(
+          pattern,
+          multiLine: isMultiLine,
+          caseSensitive: isCaseSensitive,
+          dotAll: isDotAll,
+          unicode: isUnicode,
+        ),
+        onMatch: (Match match) {
+          final matchText = match[0];
+
+          final mapping = _mapping[matchText!] ??
+              _mapping[_mapping.keys.firstWhere((element) {
+                final reg = RegExp(
+                  element,
+                  multiLine: isMultiLine,
+                  caseSensitive: isCaseSensitive,
+                  dotAll: isDotAll,
+                  unicode: isUnicode,
+                );
+                return reg.hasMatch(matchText);
+              }, orElse: () {
+                return '';
+              })];
+
+          InlineSpan widget;
+
+          if (mapping != null) {
+            if (mapping.renderText != null) {
+              Map<String, String> result =
+                  mapping.renderText!(str: matchText, pattern: pattern);
+
+              widget = TextSpan(
+                text: "${result['display']}",
+                style: mapping.style != null ? mapping.style : style,
+                recognizer: TapGestureRecognizer()
+                  ..onTap = () => mapping.onTap!(matchText),
               );
-              return reg.hasMatch(matchText);
-            }, orElse: () {
-              return '';
-            })];
-
-        InlineSpan widget;
-
-        if (mapping != null) {
-          if (mapping.renderText != null) {
-            Map<String, String> result =
-                mapping.renderText!(str: matchText, pattern: pattern);
-
-            widget = TextSpan(
-              text: "${result['display']}",
-              style: mapping.style != null ? mapping.style : style,
-              recognizer: TapGestureRecognizer()
-                ..onTap = () => mapping.onTap!(matchText),
-            );
-          } else if (mapping.renderWidget != null) {
-            widget = WidgetSpan(
-              alignment: PlaceholderAlignment.middle,
-              child: GestureDetector(
-                onTap: () => mapping.onTap!(matchText),
-                child: mapping.renderWidget!(
-                    text: matchText, pattern: mapping.pattern!),
-              ),
-            );
+            } else if (mapping.renderWidget != null) {
+              widget = WidgetSpan(
+                alignment: PlaceholderAlignment.middle,
+                child: GestureDetector(
+                  onTap: () => mapping.onTap!(matchText),
+                  child: mapping.renderWidget!(
+                      text: matchText, pattern: mapping.pattern!),
+                ),
+              );
+            } else {
+              widget = TextSpan(
+                text: "$matchText",
+                style: mapping.style != null ? mapping.style : style,
+                recognizer: TapGestureRecognizer()
+                  ..onTap = () => mapping.onTap!(matchText),
+              );
+            }
           } else {
             widget = TextSpan(
               text: "$matchText",
-              style: mapping.style != null ? mapping.style : style,
-              recognizer: TapGestureRecognizer()
-                ..onTap = () => mapping.onTap!(matchText),
+              style: style,
             );
           }
-        } else {
-          widget = TextSpan(
-            text: "$matchText",
-            style: this.style,
-          );
-        }
 
-        widgets.add(widget);
+          widgets.add(widget);
 
-        return '';
-      },
-      onNonMatch: (String text) {
-        widgets.add(TextSpan(
-          text: "$text",
-          style: this.style,
-        ));
+          return '';
+        },
+        onNonMatch: (String text) {
+          widgets.add(TextSpan(
+            text: "$text",
+            style: style,
+          ));
 
-        return '';
-      },
-    );
+          return '';
+        },
+      );
 
-    if (selectable) {
-      return SelectableText.rich(
-        TextSpan(children: <InlineSpan>[...widgets], style: style),
-        maxLines: maxLines,
-        strutStyle: strutStyle,
-        textWidthBasis: textWidthBasis,
-        textAlign: alignment,
-        textDirection: textDirection,
-        onTap: onTap as void Function()?,
+      if (widget.selectable) {
+        return SelectableText.rich(
+          TextSpan(children: <InlineSpan>[...widgets], style: style),
+          maxLines: widget.maxLines,
+          strutStyle: widget.strutStyle,
+          textWidthBasis: widget.textWidthBasis,
+          textAlign: widget.alignment,
+          textDirection: widget.textDirection,
+          onTap: widget.onTap as void Function()?,
+        );
+      }
+
+      // return RichText(
+      //   softWrap: widget.softWrap,
+      //   overflow: widget.overflow,
+      //   textScaleFactor: widget.textScaleFactor,
+      //   maxLines: widget.maxLines,
+      //   strutStyle: widget.strutStyle,
+      //   textWidthBasis: widget.textWidthBasis,
+      //   textAlign: widget.alignment,
+      //   textDirection: widget.textDirection,
+      //   text: TextSpan(
+      //     text: '',
+      //     children: <InlineSpan>[...widgets],
+      //     style: style,
+      //   ),
+      // );
+
+      return Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          RichText(
+            softWrap: widget.softWrap,
+            overflow: widget.overflow,
+            textScaleFactor: widget.textScaleFactor,
+            maxLines: widget.maxLines,
+            strutStyle: widget.strutStyle,
+            textWidthBasis: widget.textWidthBasis,
+            textAlign: widget.alignment,
+            textDirection: widget.textDirection,
+            text: TextSpan(
+              text: '',
+              children: <InlineSpan>[...widgets],
+              style: style,
+            ),
+          ),
+          widget.isLoadMore && isShowReadMore
+              ? InkWell(
+                  onTap: _onTapLink,
+                  child: !_readMore
+                      ? Text(
+                          widget.trimExpandedText,
+                          style: widget.lessStyle,
+                        )
+                      : Text(
+                          widget.trimCollapsedText,
+                          style: widget.moreStyle,
+                        ),
+                )
+              : Container(),
+        ],
+      );
+    });
+
+    if (widget.semanticsLabel != null) {
+      result = Semantics(
+        textDirection: widget.textDirection,
+        label: widget.semanticsLabel,
+        child: ExcludeSemantics(
+          child: result,
+        ),
       );
     }
-
-    return RichText(
-      softWrap: softWrap,
-      overflow: overflow,
-      textScaleFactor: textScaleFactor,
-      maxLines: maxLines,
-      strutStyle: strutStyle,
-      textWidthBasis: textWidthBasis,
-      textAlign: alignment,
-      textDirection: textDirection,
-      text: TextSpan(
-        text: '',
-        children: <InlineSpan>[...widgets],
-        style: style,
-      ),
-    );
+    return result;
   }
 }
